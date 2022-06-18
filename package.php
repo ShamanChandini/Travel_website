5126ab7d57a44c74253279ada28b5310d62d3819
Shaman Chandini <89247662+ShamanChandini@users.noreply.github.com>
2022-06-18 16:24:48 +0530

    Add files via upload

---
+++ package.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>package</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+<div class="heading" style="background:url(https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwallpaperswide.com%2Fdownload%2Ftravel-wallpaper-2560x768.jpg&f=1&nofb=1) no-repeat">
+   <h1>packages</h1>
+</div>
+<section class="packages">
+
+   <h1 class="heading-title">top destinations</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FPwUVti9aEx0%2Fmaxresdefault.jpg&f=1&nofb=1" alt="">
+         </div>
+         <div class="content">
+            <h3>CHENNAI</h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkeepcalmandwander.com%2Fwp-content%2Fuploads%2F2017%2F02%2FJama-Masjid-Delhi-India-2-960x639.jpg&f=1&nofb=1" alt="">
+         </div>
+         <div class="content">
+            <h3>DELHI</h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fwww.sweetannu.com%2Fwp-content%2Fuploads%2F2019%2F06%2FMysore1.jpg%3Fresize%3D1140%252C642%26ssl%3D1&f=1&nofb=1" alt="">
+         </div>
+         <div class="content">
+            <h3>BANGALORE</h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp3394397.jpg&f=1&nofb=1" alt="">
+         </div>
+         <div class="content">
+            <h3>KERALA</h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.caleidoscope.in%2Fwp-content%2Fuploads%2F2018%2F09%2F1-Gateway-Of-India.jpg&f=1&nofb=1" alt="">
+         </div>
+         <div class="content">
+            <h3>MUMBAI</h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fwww.southtourism.in%2Fblog%2Fwp-content%2Fuploads%2F2017%2F11%2FRushikonda-beach-1.jpg%3Fssl%3D1&f=1&nofb=1" alt="">
+         </div>
+         <div class="content">
+            <h3>VIZAG</h3>
+            <a href="book.html" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+</section><section class="footer">
+
+     <div class="credit"> created by <span>Shaman Chandini</span> | all rights reserved! </div>
+</section>
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
